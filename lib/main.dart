458f25c856eac07965f48a1dc9314156f3902b4e
cbabi2023 <abijithcbbsc2021@gmail.com>
2024-08-08 09:37:33 +0530

    images adding

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,7 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Image.network(
+              "https://images.pexels.com/photos/25287581/pexels-photo-25287581/free-photo-of-black-stool-in-front-of-green-backdrop.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+        ),
+      ),
     );
   }
 }
